cfb18f457bc5fa0d57fdb56b60ed6f0d0e7f22f9
Tomokatsu Iguchi <tomo14916253@gmail.com>
2017-02-09 22:55:00 +0900

    firebaseObject firebaseArray

--- public/app.js
+++ public/app.js
@@ -8,10 +8,31 @@
     };
   firebase.initializeApp(config);
 
-  angular.module('app', ['firebase'])
-  .controller('MyCtrl', function($firebaseObject){
+  angular
+  .module('app', ['firebase'])
+  .controller('MyCtrl', function($firebaseObject, $firebaseArray, $scope){
       const rootRef = firebase.database().ref().child('angular');
       const ref = rootRef.child('object');
+      const refData = rootRef.child('data');
+      const refTalks = rootRef.child('talks');
+      // [Object]
+      // download the data into a local object
       this.object = $firebaseObject(ref);
+      const syncData = $firebaseObject(refData);
+       // 3 way data binding!!!!
+      syncData.$bindTo($scope, "data");
+
+      // [Array] read-only array
+      // list should never be modified directly by using array methods like push() or splice()
+      // Instead, AngularFire provides a set of methods compatible with manipulating synchronized arrays:
+      //  $add(), $save(), and $remove()
+      $scope.talks = $firebaseArray(refTalks);
+      //
+      $scope.addTalk = function() {
+        console.log('addTalk');
+        $scope.talks.$add({
+          text: $scope.newTalk
+        });
+      };
   })
-})
+}())
